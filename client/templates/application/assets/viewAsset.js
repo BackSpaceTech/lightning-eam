0d373c90a2f5d1abcbb2a18d454c7139858a068d
Paul Coady <asia.pacific.robotics@gmail.com>
2015-12-21 15:20:47 +1100

    View Locations and Assets with FAB to edit

--- client/templates/application/assets/viewAsset.js
+++ client/templates/application/assets/viewAsset.js
@@ -1,12 +1,41 @@
-Template.viewAsssetPage.onCreated(function() {
+Template.viewAssetPage.onCreated(function() {
   var self = this;
    self.autorun(function() {
      self.subscribe('singleLocation', Session.get("idTreeView").toString());
    });
 });
 
-Template.viewAsssetPage.helpers({
+Template.viewAssetPage.helpers({
   viewDoc: function() {
     return Locations.findOne({"id":Session.get("idTreeView").toString()});
+  }
+});
+
+Template.viewAssetPage.events({
+  'click #btnViewAssetPageEdit': function(e) {
+    if (Session.get("idTreeView").toString()=== "#"){
+      alert("No location or asset selected!")
+    }
+    else{
+      if (Locations.findOne({"id":Session.get("idTreeView").toString()}).type === "asset"){
+        FlowRouter.go('/assets/edit-asset')
+      }
+      else{
+        FlowRouter.go('/assets/edit-location')
+      }
+    }
+  },
+  'click #btnViewAssetPageCopy': function(e) {
+    if (Session.get("idTreeView").toString()=== "#"){
+      alert("No location or asset selected!")
+    }
+    else{
+      if (Locations.findOne({"id":Session.get("idTreeView").toString()}).type === "asset"){
+        FlowRouter.go('/assets/duplicate-asset')
+      }
+      else{
+        FlowRouter.go('/assets/duplicate-location')
+      }
+    }
   }
 });
